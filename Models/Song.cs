263f1077c17cbf9b80219aa654329a6ce5d775de
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-14 21:28:30 +0400

    Initial commit

---
+++ Models/Song.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14AugHomeWork.Models
+{
+    class Song
+    {
+        private string _name;
+        private string _genre;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set 
+            {
+                if (value.Length<=100)
+                {
+                    _name = value;
+                }
+            }
+        }
+        public string  Genre 
+        {
+            get 
+            {
+                return _genre;
+            }
+            set
+            {
+                if (value == "Pop")
+                {
+                    _genre = value;
+                }
+                else if (value == "Jazz")
+                {
+                    _genre = value;
+                }
+                else if (value == "Rock")
+                {
+                    _genre = value;
+                }
+                else if (value == "Techno") 
+                {
+                    _genre = value;
+                }
+            }
+        }
+        public Singer Singer { get; set; }
+        private decimal _oldRating;
+        private decimal _currentRating;
+        private decimal _avarageRating;
+        private int _countRating = 0;
+        private decimal _sumRatings = 0;
+
+        public void AddRating(decimal Rating) 
+        {
+            _oldRating = _currentRating;
+            
+            _currentRating = Rating;
+            _countRating++;
+            _sumRatings = _sumRatings + _currentRating;
+            
+        }
+        #region Just for get current Rating
+
+        //public decimal GetAverageRating()
+        //{
+        //    return _currentRating;
+        //}
+        #endregion
+        public void FindAvarageRating() 
+        {
+            _avarageRating = _sumRatings  / _countRating;
+        }
+      
+        public decimal GetAverageRating() 
+        {
+           
+            return _avarageRating;
+            
+
+        }
+        public void ShowInfo() 
+        {
+            Console.WriteLine($"Music name:{Name} \n Music genre:{Genre} \n Singer name:{Singer.Name} \n Singer surname:{Singer.SurName} \n Singer age:{Singer.Age} \n Avarage Rating: {_avarageRating} ");
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using _14AugHomeWork.Models;
+using System;
+
+namespace _14AugHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name of Singer:");
+            string nameSinger = Console.ReadLine();
+            Console.Write("Enter surname of Singer:");
+            string surNameSing = Console.ReadLine();
+            Console.Write("Enter the age of Singer:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Singer SingerNo1 = new Singer
+            {
+                Name = nameSinger,
+                SurName= surNameSing,
+                Age = age
+            };
+            Console.Write("Enter the song name:");
+            string nameSong = Console.ReadLine();
+            Console.Write("Enter the song genre:");
+            string genreSong = Console.ReadLine();
+
+
+            Song SongNo1 = new Song()
+            {
+                Name = nameSong,
+                Genre = genreSong,
+                Singer = SingerNo1
+                
+            };
+            int purpose = 1;
+            while (purpose==1)
+            {
+                Console.WriteLine("Enter your rating for song:");
+                decimal rating = Convert.ToDecimal(Console.ReadLine());
+                SongNo1.AddRating(rating);
+                Console.WriteLine($"Do you want to add new rating? \n " +
+                    "  If you want exit press 0 \n  " +
+                    "  If you want contunie press 1 \n  " +
+                    "Please do not touch other buttons  ");
+                purpose = Convert.ToInt32(Console.ReadLine());
+                SongNo1.FindAvarageRating();
+            }
+
+
+
+
+
+            #region Without Readline
+            //SongNo1.AddRating(4);
+            //SongNo1.AddRating(3);
+            //SongNo1.AddRating(7);
+            //SongNo1.FindAvarageRating();
+            //Console.WriteLine(SongNo1.GetAverageRating());
+            #endregion
+            SongNo1.ShowInfo();
+
+        }
+    }
+}
